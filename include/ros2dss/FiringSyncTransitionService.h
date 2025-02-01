eeafddf0975561ff31e6862fb9848dd4ffa89dad
abid <chiheb.abid@gmail.com>
2025-02-01 01:46:47 +0100

    Implement firingSyncTransitionsService method and update service initialization logic

--- include/ros2dss/FiringSyncTransitionService.h
+++ include/ros2dss/FiringSyncTransitionService.h
@@ -10,6 +10,7 @@ public:
 
 
 private:
+    void firingSyncTransitionsService(const std::shared_ptr<ros2dss::FiringSyncTransition::Request>,std::shared_ptr<ros2dss::FiringSyncTransition::Response> );
     bool m_should_shutdown {false};
     dss::PetriNet *m_petri;
     rclcpp::Service<ros2dss::FiringSyncTransition>::SharedPtr m_server_firing_service {};
--- src/FiringSyncTransitionService.cpp
+++ src/FiringSyncTransitionService.cpp
@@ -2,5 +2,21 @@
 // Created by chiheb on 01/02/25.
 //
 #include "gmisc.h"
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+
 FiringSyncTransitionService::FiringSyncTransitionService(dss::PetriNet  *petri):Node("firing_sync_transitions"),m_petri(petri) {
+  if (m_petri->getPetriID()==0) {
+    m_client_firing_service=create_client<ros2dss::FiringSyncTransition>("firing_sync_transitions_service_client");
+    }
+    else {
+        m_server_firing_service=create_service<ros2dss::FiringSyncTransition>("firing_sync_transitions_service",std::bind(&FiringSyncTransitionService::firingSyncTransitionsService,this,_1,_2));
+    }
+}
+
+void FiringSyncTransitionService::firingSyncTransitionsService(const std::shared_ptr<ros2dss::FiringSyncTransition::Request> req,
+                                  std::shared_ptr<ros2dss::FiringSyncTransition::Response> resp) {
+
+
 }
